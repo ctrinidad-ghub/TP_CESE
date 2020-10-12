f17ad5d93a3ec329c8d5a06435c9ee490e166a90
Cristian Trinidad <trinidadcristian@yahoo.com.ar>
2020-10-12 13:34:57 -0300

    ADC DMA agregado

--- inc/app_adc.h
+++ inc/app_adc.h
@@ -16,23 +16,33 @@
 #include "driver/gpio.h"
 #include "driver/timer.h"
 
-// ADC
-#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES   64          //Multisampling
+//#define USE_DMA
 
-#define ADC_CHANNELS 4
-#define SC_CH ADC_CHANNEL_6
-#define PV_CH ADC_CHANNEL_7
-#define SV_CH ADC_CHANNEL_4
-#define PC_CH ADC_CHANNEL_5
-#define ATTEN ADC_ATTEN_DB_11
+//i2s sample rate
+#define I2S_SAMPLE_RATE   (8000)
+
+//I2S read buffer length
+#define I2S_READ_LEN      (1024)
+
 
 // TMR
 #define TIMER_DIVIDER         16  //  Hardware timer clock divider
 #define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
-#define TIMER_INTERVAL0_USEC  (200)    // test interval for the timer in us
+#define TIMER_INTERVAL0_USEC  (50)     // test interval for the timer in us
 #define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
 #define TEST_WITH_RELOAD      1        // testing will be done with auto reload
+
+// ADC
+#define DEFAULT_VREF    1100
+#define NO_OF_SAMPLES   20000 / (TIMER_INTERVAL0_USEC)
+
+#define ADC_CHANNELS 4
+#define NO_OF_SAMPLES_PRE_CH   NO_OF_SAMPLES / ADC_CHANNELS
+#define SC_CH ADC1_CHANNEL_6
+#define PV_CH ADC1_CHANNEL_7
+#define SV_CH ADC1_CHANNEL_4
+#define PC_CH ADC1_CHANNEL_5
+#define ATTEN ADC_ATTEN_DB_11
 
 typedef enum {
 	PV, PC, SV, SC
@@ -46,7 +56,6 @@ typedef struct {
 	adc_channel_t channel;
 	uint32_t sum_voltage;
 	uint32_t rms;
-	esp_adc_cal_characteristics_t *adc_chars;
 } adc_t;
 
 /*
--- src/app_adc.c
+++ src/app_adc.c
@@ -15,6 +15,7 @@
 #include "../inc/app_adc.h"
 #include "esp_adc_cal.h"
 #include "driver/timer.h"
+#include "driver/i2s.h"
 
 /*=====[Definition macros of private constants]==============================*/
 
@@ -27,9 +28,85 @@ uint32_t Vp = 0, Vs = 0, Ip = 0, Is = 0;
 /*=====[Definitions of private global variables]=============================*/
 
 static adc_t adc[ADC_CHANNELS];
+static esp_adc_cal_characteristics_t *adc_chars;
 
 /*=====[Definitions of internal functions]===================================*/
 
+#ifdef USE_DMA
+uint16_t i2s_read_buff_[I2S_READ_LEN];
+
+void appAdcDmaInit()
+{
+	 i2s_config_t i2s_config = {
+        .mode = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN,
+        .sample_rate =  I2S_SAMPLE_RATE,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+	    .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+	    .channel_format = I2S_CHANNEL_FMT_ALL_LEFT,
+	    .intr_alloc_flags = 0,
+	    .dma_buf_count = 2,
+	    .dma_buf_len = 1024,
+	    .use_apll = 0,
+	 };
+
+	 //install and start i2s driver
+	 i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+
+	 //init ADC pad
+	 i2s_set_adc_mode(ADC_UNIT_1, adc[0].channel);
+}
+
+void adc_dma(void*arg)
+{
+
+    size_t bytes_read;
+    uint32_t i;
+    uint32_t adcIndex = 0;
+
+    // Read from ADC and save
+    uint16_t* i2s_read_buff = i2s_read_buff_;
+
+    while (1) {
+		i2s_adc_enable(I2S_NUM_0);
+
+		//read data from I2S bus, in this case, from ADC.
+		i2s_read(I2S_NUM_0, (uint16_t*) i2s_read_buff, I2S_READ_LEN * sizeof(uint16_t), &bytes_read, portMAX_DELAY);
+
+		i2s_adc_disable(I2S_NUM_0);
+
+		for (i=0;i<I2S_READ_LEN;i++){
+			uint16_t voltage = esp_adc_cal_raw_to_voltage(i2s_read_buff_[i] & 0x0FFF, adc_chars);
+			adc[adcIndex].sum_voltage += voltage;
+		}
+
+		adc[adcIndex].rms = adc[adcIndex].sum_voltage/I2S_READ_LEN;
+		switch(adcIndex){
+			case 0:
+				Vp = adc[adcIndex].rms;
+				break;
+			case 1:
+				Vs = adc[adcIndex].rms;
+				break;
+			case 2:
+				Ip = adc[adcIndex].rms;
+				break;
+			case 3:
+				Is = adc[adcIndex].rms;
+				break;
+		}
+		adc[adcIndex].sum_voltage = 0;
+		if (adcIndex == ADC_CHANNELS-1) adcIndex = 0;
+		else adcIndex++;
+
+		i2s_set_adc_mode(ADC_UNIT_1, adc[adcIndex].channel);
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+#else
 /*
  * Timer group0 ISR handler
  *
@@ -119,14 +196,14 @@ void periodic_task(void *arg)
         adc_reading = adc1_get_raw((adc1_channel_t) adc[adcIndex].channel);
 
         //Convert adc_reading to voltage in mV
-        voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc[adcIndex].adc_chars);
+        voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
 
         /*voltage = voltage * voltage;
         adc[adcIndex].sum_voltage += voltage;
         i++;
-        if (i == (20000 / (TIMER_INTERVAL0_USEC))){
+        if (i == NO_OF_SAMPLES){
         	i = 0;
-        	adc[adcIndex].sum_voltage /= (20000 / (TIMER_INTERVAL0_USEC));
+        	adc[adcIndex].sum_voltage /= NO_OF_SAMPLES_PRE_CH;
         	//printf("%d\n", sum_voltage);
         	rms = sqrt(sum_voltage);
         	adc[adcIndex].sum_voltage = 0;
@@ -158,11 +235,15 @@ void periodic_task(void *arg)
     }
 }
 
+#endif
+
 /*=====[Definitions of external functions]===================================*/
 
 void appAdcInit(void)
 {
+#ifndef USE_DMA
 	TaskHandle_t handle_periodic_task;
+#endif
 	uint32_t adcIndex = 0;
 
 	adc[0].channel = (adc_channel_t) PV_CH;
@@ -170,19 +251,27 @@ void appAdcInit(void)
 	adc[2].channel = (adc_channel_t) SV_CH;
 	adc[3].channel = (adc_channel_t) SC_CH;
 
+	adc1_config_width(ADC_WIDTH_BIT_12);
+
 	for (adcIndex = 0; adcIndex < ADC_CHANNELS; adcIndex++)
 	{
+		adc[adcIndex].sum_voltage = 0;
+		adc[adcIndex].rms = 0;
 		//Configure ADC
-		adc1_config_width(ADC_WIDTH_BIT_12);
 		adc1_config_channel_atten(adc[adcIndex].channel, (adc_atten_t) ATTEN);
-
-		//Characterize ADC
-		adc[adcIndex].adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-		esp_adc_cal_characterize((adc_unit_t) ADC_UNIT_1, (adc_atten_t) ATTEN, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc[adcIndex].adc_chars);
 	}
 
+	//Characterize ADC
+	adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+	esp_adc_cal_characterize((adc_unit_t) ADC_UNIT_1, (adc_atten_t) ATTEN, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+
+#ifdef USE_DMA
+	appAdcDmaInit();
+	xTaskCreate(adc_dma, "adc_dma", 1024 * 2, NULL, 5, NULL);
+#else
 	xTaskCreate(periodic_task, "timer_periodic_task", 2048, NULL, 5, &handle_periodic_task);
 	app_timer_init((timer_idx_t) TIMER_0, TEST_WITH_RELOAD, TIMER_INTERVAL0_USEC, &handle_periodic_task);
+#endif
 }
 
 
